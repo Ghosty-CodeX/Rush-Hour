1775d93d9009bc7e98195acfa3ad6f749caaa49f
Ghosty-CodeX <brandonbosch8@gmail.com>
2025-05-20 14:45:49 +0400

    Player movement
    Camera and player movement added

--- player.gd
+++ player.gd
@@ -1,4 +1,17 @@
 extends CharacterBody3D
 
-@export var forward_speed = 20
-@export var lane_width = 4.0
+@export var move_speed: float = 10.0
+@export var strafe_speed: float = 5.0
+
+var input_direction: float = 0.0
+
+func _physics_process(delta):
+	#constant movemant forward
+	velocity.z = -move_speed
+	
+	#left and right input
+	input_direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	velocity.x = input_direction * strafe_speed
+	
+	#apply movement
+	move_and_slide()
